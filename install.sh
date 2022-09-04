1e3c3eb24e0dac4e8f4ecf4c57791a5be2a14df7
Bangs Ngek <70790541+BangsNgek@users.noreply.github.com>
2022-09-04 22:31:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,8 +13,8 @@ rm nsdomain
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 subsl=$(</dev/urandom tr -dc a-x0-9 | head -c5)
 DOMAIN=bangsngeks.my.id
-SUB_DOMAIN=sg-${sub}.mantapxsl.my.id
-NS_DOMAIN=slowdns-${subsl}.mantapxsl.my.id
+SUB_DOMAIN=sg-${sub}.bangsngeks.my.id
+NS_DOMAIN=slowdns-${subsl}.bangsngeks.my.id
 CF_ID=bangsngek@gmail.com
 CF_KEY=9b8c1ae3cbbe24bcf77b7d0c4b50d60ebb087
 echo "IP=""$SUB_DOMAIN" >> /var/lib/crot/subdomain.conf
