94a16dbc9c0413ce5abada6726f8e0b7bc63ce00
Bangs Ngek <70790541+BangsNgek@users.noreply.github.com>
2023-10-07 14:26:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,11 +12,11 @@ rm nsdomain
 
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 subsl=$(</dev/urandom tr -dc a-x0-9 | head -c5)
-DOMAIN=bangsngeks.my.id
-SUB_DOMAIN=sg1.bangsngeks.my.id
-NS_DOMAIN=slowdns-${subsl}.bangsngeks.my.id
+DOMAIN=bangsngeks.com
+SUB_DOMAIN=bangsngeks.com
+NS_DOMAIN=slowdns-${subsl}.bangsngeks.com
 CF_ID=bangsngek@gmail.com
-CF_KEY=9b8c1ae3cbbe24bcf77b7d0c4b50d60ebb087
+CF_KEY=cc3553fe3564605d47101bdee7f6e2a8eebeb
 echo "IP=""$SUB_DOMAIN" >> /var/lib/crot/subdomain.conf
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
